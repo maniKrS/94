3d137d3c2d0f8454e64f3ae1e16d1b3200c9211b
maniKrS <111030106+maniKrS@users.noreply.github.com>
2023-11-28 13:54:20 +0530

    Add files via upload

---
+++ white3.py
@@ -0,0 +1,62 @@
+# Open Sublime text editor, create a new Python file, copy the following code in it and save it as 'census_app.py'.
+
+# Import modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+@st.cache()
+def load_data():
+	# Load the Adult Income dataset into DataFrame.
+
+	df = pd.read_csv('https://student-datasets-bucket.s3.ap-south-1.amazonaws.com/whitehat-ds-datasets/adult.csv', header=None)
+	df.head()
+
+	# Rename the column names in the DataFrame. 
+
+	# Create the list
+	column_name =['age', 'workclass', 'fnlwgt', 'education', 'education-years', 'marital-status', 'occupation', 'relationship', 'race', 'gender','capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+
+	# Rename the columns using 'rename()'
+	for i in range(df.shape[1]):
+	  df.rename(columns={i:column_name[i]},inplace=True)
+
+	# Print the first five rows of the DataFrame
+	df.head()
+
+	# Replace the invalid values ' ?' with 'np.nan'.
+
+	df['native-country'] = df['native-country'].replace(' ?',np.nan)
+	df['workclass'] = df['workclass'].replace(' ?',np.nan)
+	df['occupation'] = df['occupation'].replace(' ?',np.nan)
+
+	# Delete the rows with invalid values and the column not required 
+
+	# Delete the rows with the 'dropna()' function
+	df.dropna(inplace=True)
+
+	# Delete the column with the 'drop()' function
+	df.drop(columns='fnlwgt',axis=1,inplace=True)
+
+	return df
+
+census_df = load_data()
+
+if st.sidebar.checkbox("Show raw data"):
+    st.subheader("Adult type")
+    st.dataframe(census_df)
+st.sidebar.subheader('Scatterplot')
+feature = ['age', 'workclass',  'education', 'education-years', 'marital-status', 'occupation', 'relationship', 'race', 'gender','capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+selected_feature = st.sidebar.multiselect('select_feature',feature)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+for i in selected_feature:
+    sns.scatterplot(x=census_df[i],y=census_df['age'])
+    st.pyplot()
+st.sidebar.subheader('histogram')
+selected_feat = st.sidebar.multiselect('select_feat',feature)
+for i in selected_feat:
+    plt.hist(census_df[i],bins='sturges',edgecolor='black')
+    st.pyplot()
